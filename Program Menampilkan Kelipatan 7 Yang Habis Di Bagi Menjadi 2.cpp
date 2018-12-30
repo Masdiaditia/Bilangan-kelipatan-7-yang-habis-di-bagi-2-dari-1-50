34f6f9e774e60995ee40970e3666649269ea6440
Masdi Aditia <45525790+Masdiaditia@users.noreply.github.com>
2018-12-30 08:10:16 +0700

    Add files via upload

---
+++ Program Menampilkan Kelipatan 7 Yang Habis Di Bagi Menjadi 2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int main ();
+    printf ("Kelipatan & Yang Habis Di Bagi 2(1=50).\n\n");
+    int i=7;
+    while(i<50)
+    {
+        if (i%2==0&i%7==0)
+        {
+            printf("\n%d",i);
+
+        }   i++;
+
+    }
+    getch();
+
+}
